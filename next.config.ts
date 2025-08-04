9870ff576b1d5f58db6c324c80aa163f7c83a6aa
Morgan Pritchard <morgan.pritchard@fhnw.edu>
2025-08-04 17:11:23 -0600

    Added Github Pages true statement log

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const isGithubPages = process.env.GITHUB_PAGES === "true";
+console.log("🔍 GITHUB_PAGES =", process.env.GITHUB_PAGES);
 
 const nextConfig: NextConfig = {
   output: "export",
